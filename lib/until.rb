42381f04e5ed849022ddb1ffe5090b2dba5f4ffb
Karen Daniela <kdaniimora@gmail.com>
2019-12-15 18:29:53 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,13 +1,11 @@
 def using_until
   levitation_force = 6
-while 
-levitation_force < 10	
-	Wingandium_Leviosa = +1 
-	puts "Wingandium Leviosa {Wingandium Leviosa}" 
-  #your code here
-end
+while levitation_force < 10	do
+	puts "Wingandium Leviosa
+		Wingandium_Leviosa = +1 
+ end
 
-puts levitation_force
+puts levitation_force = 10 
 
     
 end
